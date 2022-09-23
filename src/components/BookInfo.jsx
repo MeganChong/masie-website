ee694b64befabacb4e58a423972c7ca168e811f6
LAPTOP-VGOFIN0E\Megan <mchong5@gmu.edu>
2022-09-22 22:35:37 -0400

    set up functions and vars skeleton for book info

--- src/components/BookInfo.jsx
+++ src/components/BookInfo.jsx
@@ -8,18 +8,48 @@ import './BookInfo.css';
 //***or data.projects[title]
 
 export default function BookInfo() {
+    var UTRbg = "rgb(0, 128, 0)";
+    var LTRbg = "rgb(255, 192, 203)";
+    var barbg = "rgb(173, 216, 230)";
+    var carouselbg = "rgb(128, 0, 128)";
+
+    function upperTriangle() {
+        return ( <div style={{backgroundColor: `${UTRbg}`}} className="upperTriangle"></div> );
+    }
+
+    function lowerTriangle() {
+        return ( <div style={{backgroundColor: `${LTRbg}`}} className="lowerTriangle"></div> );
+    }
+
+    function bottom() {
+        return ( <div style={{backgroundColor: `${LTRbg}`, height: "70vh"}}></div> );
+    }
+
+    function bottomBar() {
+        return ( <div style={{backgroundColor: `${barbg}`, height: "3vh"}}></div> );
+    }
+
+    function checkMore() {
+        return (
+            <p style={{padding: "3%", height: "15vh", textAlign: "center"}}>
+                CHECK OUT MORE
+            </p>
+        );
+    }
+
+    function carousel() {
+        return ( <div style={{backgroundColor: `${carouselbg}`, height: "85vh"}}></div> );
+    }
 
     function displayPage2() {
         return (
             <div className="parent">
-                <div className="upperTriangle"></div>
-                <div className="lowerTriangle"></div>
-                <div className="bottom"></div>
-                <div className="endBar"></div>
-                <div className="checkMore">
-                    <p style={{padding: "3%"}}>CHECK OUT MORE</p>
-                </div>
-                <div className="carousel"></div>
+                {upperTriangle()}
+                {lowerTriangle()}
+                {bottom()}
+                {bottomBar()}
+                {checkMore()}
+                {carousel()}
             </div>
         );
     }
