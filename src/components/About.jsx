9347b098b5d43ec2115541be216dc435fbee323d
LAPTOP-VGOFIN0E\Megan <mchong5@gmu.edu>
2022-10-13 18:46:45 -0400

    have about page use side nav bar

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,25 +3,17 @@ import NavBar from "./NavBars/NavBar";
 
 export default function About() {
     return (
-        <div>
-            <NavBar show={true}/>
+        <div class="min-h-screen md:flex">
+            <NavBar show={true} displayType={"category"}/>
 
-            <div class="bg-about bg-cover min-h-screen min-w-min">
-                <p class="text-white text-left text-about 
-                        px-8 pt-24 mx-8
-                        md:pl-32 md:pt-28
-                        lg:pl-48 lg:pt-32 lg:pr-96 lg:mr-56">
-                    Hi there! <br/>
-                    I see design as an opportunity 
-                    to give an idea a beautiful and unique visual identity 
-                    that stands out from the crowd. 
-                    I love to work with color and am passionate in exploring 
-                    all the different places design takes me. 
-                    As a print and digital designer, 
-                    I am experienced in designing for advertising, publications, conferences, 
-                    social media campaigns and more. 
-                    Please feel free to contact me at sleepingartist693@gmail.com. 
-                    Thank you for visiting my website!
+            <div class="bg-about bg-cover min-h-screen">
+                <p class="text-white text-left text-about
+                            px-8 pt-24 pb-10 mx-8 
+                            md:pl-32 md:pt-28
+                            lg:pl-48 lg:pt-32 lg:pr-32 lg:mr-56">
+                    My name is Masie Chong. Each week one graphic designer tries to take me 
+                    down in my house. If you think you have what it takes to beat me 
+                    contact me at sleepingartist693@gmail.
                 </p>
             </div>
         </div>
