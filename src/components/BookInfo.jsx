73055580a15671a01e306fff19a682dc974a7bb5
LAPTOP-VGOFIN0E\Megan <mchong5@gmu.edu>
2022-10-13 13:40:12 -0400

    added grid for mobile design for text and process

--- src/components/BookInfo.jsx
+++ src/components/BookInfo.jsx
@@ -88,13 +88,42 @@ export default function BookInfo() {
                 {/* empty div to move the rest of the items down to correct position */}
                 <div style={{height: "109vh"}}></div> 
 
-                <h1 class="text-center text-5xl font-bold m-7 mb-9 mt-0" style={{whiteSpace: "pre-line"}}>
-                    {title}
-                </h1>
-                <hr className="titleLine"/>
+                    <h1 class="text-center text-5xl font-bold m-7 mb-9 mt-0" style={{whiteSpace: "pre-line"}}>
+                        {/* {title} */}
+                        The longest title in the world skdfjskdfjksnvlsnsndvjdsknvfndkanvxdsk sdncalcnadk
+                    </h1>
+                    <hr className="titleLine"/>
+                
+                
+                <div style={{height: "fit-content", backgroundColor: `${colorCodes.lowerTriangle}`}}>
 
-                <div class="flex mt-14" style={{marginLeft: "9%"}}>
-                    <div class="flex-1 p-3 pt-0">
+                    <div class="grid grid-col-1 md:grid-cols-4 mt-14 relative" 
+                        style={{marginLeft: "9%", flexWrap: "wrap"}}>
+                        <div class="col-span-1 p-3 pt-0">
+                            {showAward()}
+                            <p class="font-bold text-xl" style={{letterSpacing: "4px", textTransform: "uppercase"}}>
+                                {textDetails.header}
+                            </p>
+                            <p class="pt-3 text-lg" style={{lineHeight: "30px"}}>
+                                {textDetails.description}
+                            </p>
+                        </div>
+
+                        <div class="cols-span-1 md:col-span-3 md:ml-16 sideways">
+                            {displayProcess()}
+                        </div>
+                    </div>
+                </div>
+            </div> 
+        );
+    }
+
+    function descriptionProcess() {
+        return (
+            <div style={{height: "fit-content", backgroundColor: `${colorCodes.lowerTriangle}`}}>
+                <div class="grid grid-col-1 md:grid-cols-4 mt-14 relative" 
+                    style={{marginLeft: "9%", flexWrap: "wrap"}}>
+                    <div class="col-span-1 p-3 pt-0">
                         {showAward()}
                         <p class="font-bold text-xl" style={{letterSpacing: "4px", textTransform: "uppercase"}}>
                             {textDetails.header}
@@ -104,11 +133,11 @@ export default function BookInfo() {
                         </p>
                     </div>
 
-                    <div class="flex-1 ml-16 sideways">
+                    <div class="cols-span-1 md:col-span-3 md:ml-16 sideways">
                         {displayProcess()}
                     </div>
                 </div>
-            </div> 
+            </div>
         );
     }
 
@@ -119,8 +148,6 @@ export default function BookInfo() {
             <div className="parent">
                 <NavBar show={true} displayType={"info"} class="z-20"/>
 
-                {/* <div style={{height:"50vh", backgroundColor: "white"}}></div> */}
-
                 <div style={{height: "200vh", minHeight: "fit-content", position: "relative"}}>
                     {upperTriangle()}
                     {info()}
@@ -128,8 +155,9 @@ export default function BookInfo() {
                     {/* TODO */}
                     {/* <div className="processBox"></div> */} 
                 </div>
+                    {/* {descriptionProcess()} */}
 
-                <div style={{backgroundColor: `${colorCodes.bottomBar}`, height: "3vh"}}></div> 
+                <div style={{backgroundColor: `${colorCodes.bottomBar}`, height: "3vh", position: "relative"}}></div> 
                 <CheckOutMoreCarousel title={title}/>
             </div>
         );
