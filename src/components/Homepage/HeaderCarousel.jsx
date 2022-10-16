f072e3600fe50f0b16cb6f5f0cd7a7d578b0d4cf
LAPTOP-VGOFIN0E\Megan <mchong5@gmu.edu>
2022-10-15 20:39:23 -0400

    have homepage header be mobile responsive

--- src/components/Homepage/HeaderCarousel.jsx
+++ src/components/Homepage/HeaderCarousel.jsx
@@ -4,10 +4,10 @@ export default function HeaderCarousel() {
     function display() {
         return (
             <div className="header">
-                <div id="img1" class="slide" style={{backgroundColor: "#b7c4f7"}}>MASIE</div>
-                <div id="img2" class="slide" style={{backgroundColor: "#f4c3f7"}}>DESIGN</div>
-                <div id="img3" class="slide" style={{backgroundColor: "#fafa23"}}>IMAGINE</div>
-                <div id="img4" class="slide" style={{backgroundColor: "#c9ffd2"}}>CREATE</div>
+                <div id="img1" class="slideSm md:slideHorizontal" style={{backgroundColor: "#b7c4f7"}}>MASIE</div>
+                <div id="img2" class="slideSm md:slideHorizontal" style={{backgroundColor: "#f4c3f7"}}>DESIGN</div>
+                <div id="img3" class="slideSm md:slideHorizontal" style={{backgroundColor: "#fafa23"}}>IMAGINE</div>
+                <div id="img4" class="slideSm md:slideHorizontal" style={{backgroundColor: "#c9ffd2"}}>CREATE</div>
                 
                 <div class="downwardArrow">
                     <p>&#xFE40;</p>
