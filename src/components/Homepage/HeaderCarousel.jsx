61acc0aef13bd65b63838b8d117ceb8f423f3546
LAPTOP-VGOFIN0E\Megan <mchong5@gmu.edu>
2022-10-16 23:26:07 -0400

    added header with images

--- src/components/Homepage/HeaderCarousel.jsx
+++ src/components/Homepage/HeaderCarousel.jsx
@@ -1,13 +1,18 @@
 import "./Header.css";
 
+import carousel1 from "../../assests/homepage/header/Carousel1.jpg";
+import carousel2 from "../../assests/homepage/header/Carousel2.jpg";
+import carousel3 from "../../assests/homepage/header/Carousel3.jpg";
+import carousel4 from "../../assests/homepage/header/Carousel4.jpg";
+
 export default function HeaderCarousel() {
     function display() {
         return (
             <div className="header">
-                <div id="img1" class="slideSm md:slideHorizontal" style={{backgroundColor: "#b7c4f7"}}>MASIE</div>
-                <div id="img2" class="slideSm md:slideHorizontal" style={{backgroundColor: "#f4c3f7"}}>DESIGN</div>
-                <div id="img3" class="slideSm md:slideHorizontal" style={{backgroundColor: "#fafa23"}}>IMAGINE</div>
-                <div id="img4" class="slideSm md:slideHorizontal" style={{backgroundColor: "#c9ffd2"}}>CREATE</div>
+                <img src={carousel1} class="slide" id="img1" alt="Imagine"/>
+                <img src={carousel2} class="slide" id="img2" alt="Create"/>
+                <img src={carousel3} class="slide" id="img3" alt="Design"/>
+                <img src={carousel4} class="slide" id="img4" alt="Let's get started"/>
                 
                 <div class="downwardArrow">
                     <p>&#xFE40;</p>
