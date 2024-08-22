2cbd49ebdcad21a2cfb5d5957aa3b1994fa4854c
Masie Chong <masie.chong@MKIDDs-MBP.fios-router.home>
2024-08-21 22:34:02 -0400

    remove fade for check out more carousel

--- src/components/InteriorPage/InteriorPage.jsx
+++ src/components/InteriorPage/InteriorPage.jsx
@@ -1,16 +1,12 @@
-import React, {useState, useEffect, useMemo, useRef} from "react";
+import React, {useState, useEffect} from "react";
 import {useLocation} from "react-router";
 import CheckOutMoreCarousel from "./CheckOutMoreCarousel";
 import BookInfo from "./BookInfo";
 import ErrorPage from "./ErrorPage";
-import useIsVisible from "../useIsVisible.jsx";
 
 export default function InteriorPage() {
 	const data = useLocation();
 	const [title, setTitle] = useState("");
-	const carousel = useMemo(() => <CheckOutMoreCarousel title={title}/>, [title]);
-	const carouselRef = useRef();
-	const isCarouselVisible = useIsVisible(carouselRef);
 
 	useEffect(() => {
         	if (data === undefined || data.state === undefined || data.state.title === undefined) {
@@ -20,10 +16,7 @@ export default function InteriorPage() {
     	}, [data]);
 
 	function getCarousel() {
-		const transition = "transition-opacity ease-in duration-700";
-		const opacity = isCarouselVisible ? "opacity-100" : "opacity-0";
-		const transitionStyle = transition + " " + opacity;
-		return <div ref={carouselRef} className={transitionStyle}>{carousel}</div>
+		return <CheckOutMoreCarousel title={title}/>;
 	}
 
 	return <div className="flex flex-col">
